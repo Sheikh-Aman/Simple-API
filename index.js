9479541217dfc8e859b06f6631d2ab449939d73f
Aman Alam <shekh.aman@gmail.com>
2021-11-30 00:29:50 -0500

    added get-time api

--- index.js
+++ index.js
@@ -2,9 +2,40 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+
+  app.get('/', (req, res) => res.render('pages/index'))
+
+  app.use('/current_time', (req, res) => {
+    console.debug("request", req.headers);
+    let date = new Date();
+    const [month, day, year]       = [date.getMonth(), date.getDate(), date.getFullYear()];
+    const [hour, minutes, seconds, milliseconds, offset] = [date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds(), (date.getTimezoneOffset()/60)];
+
+    let timeStr = ""+day +"/"+(month + 1)+"/"+year+" "+hour+":"+minutes+":"+seconds+"."+milliseconds+" UTC Offset (hours): "+offset;
+
+    res.send({ date_time_str: ""+timeStr,
+                values:{
+                  day: day,
+                  month: (month + 1),
+                  year: year,
+                  hour: hour,
+                  minutes: minutes,
+                  seconds: seconds,
+                  milliseconds: milliseconds,
+                  utc_offset_hours: offset
+                },
+                headers: req.headers['cache-control']
+              });
+  });
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+module.exports = app;
